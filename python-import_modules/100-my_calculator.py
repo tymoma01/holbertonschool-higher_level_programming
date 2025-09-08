4f269e98126e7cc7a070d2c61f58f88657d854ed
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-08 15:42:05 +1000

    100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -13,12 +13,12 @@ if __name__ == "__main__":
             print("{} {} {} = {}".format(args[1], args[2], args[3], add(int(args[1]), int(args[3]))))
 
         elif args[2] == "-":
-            print("{} {} {} = {}".format(args[1], args[2], args[3], sub(args[1], args[3])))
+            print("{} {} {} = {}".format(args[1], args[2], args[3], sub(int(args[1]), int(args[3]))))
 
         elif args[2] == "/":
-            print("{} {} {} = {}".format(args[1], args[2], args[3], div(args[1], args[3])))
+            print("{} {} {} = {}".format(args[1], args[2], args[3], div(int(args[1]), int(args[3]))))
 
         elif args[2] == "*":
-            print("{} {} {} = {}".format(args[1], args[2], args[3], mul(args[1], args[3])))
+            print("{} {} {} = {}".format(args[1], args[2], args[3], mul(int(args[1]), int(args[3]))))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
