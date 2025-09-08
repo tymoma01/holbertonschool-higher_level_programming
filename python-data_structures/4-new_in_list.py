62f4443285825bb198699cb1890f6fa4037fdec0
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-08 16:06:27 +1000

    4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = []
-    for i in range(len(my_list)):
-        new_list.append(my_list[i])
+    new_list = my_list.copy()
 
     if idx < 0 or idx >= len(my_list):
         return new_list
