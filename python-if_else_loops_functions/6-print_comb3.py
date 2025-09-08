c10023eba0024c7173c3d5be4b684d6a4a357a95
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-08 11:21:46 +1000

    6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-seen_combinations = []
+
 res = ""
 for i in range(10):
     for j in range(10):
-        if i != j and f"{i}{j}" not in seen_combinations and f"{j}{i}" not in seen_combinations:
+        if i != j and f"{i}{j}" not in res and f"{j}{i}" not in res:
             res += ", {}{}".format(i, j)
-            seen_combinations.append(f"{i}{j}")
+
 
 print(res[2:])
