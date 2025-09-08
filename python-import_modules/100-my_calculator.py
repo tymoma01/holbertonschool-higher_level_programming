848ba9f86b942f47e9cdfe060649c0c4bc2032f7
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-08 15:40:56 +1000

    100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    len_args = len(sys.argv) - 1
+    args = sys.argv
+
+    if len_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    
+    else:
+        if args[2] == "+":
+            print("{} {} {} = {}".format(args[1], args[2], args[3], add(int(args[1]), int(args[3]))))
+
+        elif args[2] == "-":
+            print("{} {} {} = {}".format(args[1], args[2], args[3], sub(args[1], args[3])))
+
+        elif args[2] == "/":
+            print("{} {} {} = {}".format(args[1], args[2], args[3], div(args[1], args[3])))
+
+        elif args[2] == "*":
+            print("{} {} {} = {}".format(args[1], args[2], args[3], mul(args[1], args[3])))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
