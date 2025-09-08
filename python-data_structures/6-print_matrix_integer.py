ccb8e39b858fc00fe3a770744dfde975469b5297
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-08 16:14:36 +1000

    6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if j == len(matrix[0]) - 1:
+                print(matrix[i][j], end='')
+            else:
+                print(matrix[i][j], end=' ')
+        print("")
