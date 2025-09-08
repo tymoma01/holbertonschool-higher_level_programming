cedd4a903008f2e069720be7a49035914d43e434
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-08 13:09:10 +1000

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-add = __import__("add_0").add
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a+b)}")
+if __name__ == "__main__":
+    add = __import__("add_0").add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a+b)}")
