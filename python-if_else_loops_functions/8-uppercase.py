976a65bb0936028aa371328b59272b81ebee03fa
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-08 13:01:28 +1000

    8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     res = ""
-    for c in range(str):
+    for c in str:
         if ord(c)>=97 and ord(c)<=122:
             c = chr(ord(c)-32)
         print("{}".format(c), end='')
