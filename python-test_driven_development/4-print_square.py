6bad546f0a31e540f67cfdebf49ab10d02e939c3
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-09 17:23:40 +1000

    4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,6 +9,5 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise TypeError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-    print("\n".join("#" * size for _ in range(size)))
+    for _ in range(size):
+        print("#" * size)
