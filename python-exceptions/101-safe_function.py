2c75f4b3862684c40361fe08799d153096733382
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-09 16:15:50 +1000

    101

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 import sys
 def safe_function(fct, *args):
     try:
-        return fct(args)
+        return fct(*args)
     except Exception as e:
-        print("Exception: {e}".format(e))
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
