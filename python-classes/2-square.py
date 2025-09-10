0cc61d79c075359a8e9ca56b20541967001ced84
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-10 10:28:40 +1000

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Here is some documentation
+"""
+class Square:
+    """
+    Here is some more documentation
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
