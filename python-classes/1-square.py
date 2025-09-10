60369e8ff89792f0f319e1fd0ad7a3a47a99ee47
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-10 10:24:44 +1000

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Here is some documentation
+"""
+class Square:
+    """
+    Here is some more documentation
+    """
+    def __init__(self, size):
+        self._Square__size = size
+    
