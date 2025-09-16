7ab4961625ecbbd885e994f9b81598db0b0584e3
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-16 17:41:41 +1000

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Documentation
+"""
+
+class BaseGeometry:
+    """Basegeometry empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    
