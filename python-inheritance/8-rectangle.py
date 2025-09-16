af4c21655850eeb805be73e85d95ecd9c3ead86c
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-16 18:08:25 +1000

    8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Documentation
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle empty class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
