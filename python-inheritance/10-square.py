9c38c0e8bca041c05e4b56ac0fa14e1e90c472d2
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-17 11:58:14 +1000

    10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,10 +8,8 @@ class Square(Rectangle):
     """Square empty class"""
     def __init__(self, size):
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
         return self.__size ** 2
-    
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
