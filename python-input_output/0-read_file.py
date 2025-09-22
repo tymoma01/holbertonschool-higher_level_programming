afb22d7d8b5bb98950586833e2ebeab7cf727597
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-22 16:45:18 +1000

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@ Here is some documentation
 def read_file(filename=""):
     """Here is some more documentation"""
     with open(filename, encoding='utf-8') as f:
-        print(f.read())
+        print(f.read(), end="")
