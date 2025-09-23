46ab21497e8ff66ec4e97bcbe3f461683a438514
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-23 12:02:19 +1000

    10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -5,20 +5,14 @@
 class Student:
     """ My class
     """
-
-    score = 0
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if not attrs:
-            return self.__dict__
-
         if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
             return {attr : self.__dict__[attr] 
                     for attr in attrs 
                     if attr in self.__dict__.keys()}
-        return {}
+        return self.__dict__
