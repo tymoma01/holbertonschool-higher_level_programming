84f63950912e8032c9c6b2f11498c53ea9d15519
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-09-23 12:06:59 +1000

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" My class module
+"""
+
+class Student:
+    """ My class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr : self.__dict__[attr] 
+                    for attr in attrs 
+                    if attr in self.__dict__.keys()}
+        return self.__dict__
+    
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
